c652f7fb960a1b8e9634f4821dbd62d29e86f23b
Justin <justin@liteboxnz.com>
2024-02-03 14:58:01 +1300

    Fix observation space

--- bandits/envs/TwoArmedBanditEnv.py
+++ bandits/envs/TwoArmedBanditEnv.py
@@ -4,7 +4,7 @@ from gymnasium import spaces
 
 class TwoArmedBanditEnv(gym.Env):
 
-    def __init__(self, fixed_var=4, fixed_var_2=5, runs=100):
+    def __init__(self, fixed_var=3, fixed_var_2=7, runs=100):
         self.fixed_var = fixed_var
         self.fixed_var_2 = fixed_var_2
         self.max_runs = runs
@@ -13,14 +13,12 @@ class TwoArmedBanditEnv(gym.Env):
         self.total_reward = 0
         self.run_counter = 0
 
-        # Output should be the outcome of the choice
-        self.observation_space = spaces.Dict(
-            {
-                "total_reward": spaces.Discrete(1),
-            }
-        )
+        # Output (0-10) should be the outcome of the choice
+        self.observation_space = spaces.Dict({"total_reward": spaces.Discrete(10)})
 
-        # We have 2 options/bandits here
+        self.observation_space.sample()
+
+        # We have 2 options/bandits here [0,1]
         self.action_space = spaces.Discrete(2)
         self.render_mode = None
         self.window = None
@@ -49,8 +47,11 @@ class TwoArmedBanditEnv(gym.Env):
         else:
             reward = self.fixed_var_2
 
+        self.total_reward += reward
+
         terminated = self.run_counter > self.max_runs
         observation = self._get_obs()
+        print(observation)
         info = self._get_info()
 
         self.run_counter += 1
