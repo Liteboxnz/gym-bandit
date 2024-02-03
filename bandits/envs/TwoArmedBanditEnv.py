4aa93f42b600106364066c500dcdfeabaecfbb93
Justin <justin@liteboxnz.com>
2024-02-03 15:30:48 +1300

    Fix observation

--- bandits/envs/TwoArmedBanditEnv.py
+++ bandits/envs/TwoArmedBanditEnv.py
@@ -19,10 +19,13 @@ class TwoArmedBanditEnv(gym.Env):
         self.total_reward = 0
         self.run_counter = 0
 
-        # Output (0-10) should be the outcome of the choice
-        self.observation_space = spaces.Dict({"total_reward": spaces.Discrete(10)})
+        # No state (note 0 indexed)
+        self.observation_space = spaces.Discrete(1)
 
-        self.observation_space.sample()
+        # Output (0-10) should be the outcome of the choice
+       # self.observation_space = spaces.Dict({"total_reward": spaces.Discrete(100)})
+
+       # self.observation_space.sample()
 
         # We have 2 options/bandits here [0,1]
         self.action_space = spaces.Discrete(2)
@@ -31,7 +34,8 @@ class TwoArmedBanditEnv(gym.Env):
         self.clock = None
 
     def _get_obs(self):
-        return {"total_reward": self.total_reward}
+        return 0
+        #return {"total_reward": self.total_reward}
 
     # average award etc? debug info?
     def _get_info(self):
@@ -57,7 +61,6 @@ class TwoArmedBanditEnv(gym.Env):
 
         terminated = self.run_counter > self.max_runs
         observation = self._get_obs()
-        print(observation)
         info = self._get_info()
 
         self.run_counter += 1
